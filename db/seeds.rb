a009844904beb22220aa07d282ca2da94bb714c1
Koome <judekoome360@gmail.com>
2023-06-21 23:45:46 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+students = [
+    { first_name: "John", last_name: "Doe", grade: 10 },
+    { first_name: "Jane", last_name: "Smith", grade: 11 },
+    { first_name: "Michael", last_name: "Johnson", grade: 12 },
+    # Add more student records if needed
+  ]
+  
+  students.each do |student_params|
+    Student.create(student_params)
+  end
+  
